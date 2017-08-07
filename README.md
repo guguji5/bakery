594a0fb88103ac31f4ee1049f0620819dff7facf
guguji <kuan.du@grandinsight.com>
2017-08-07 17:12:25 +0800

    'update_readme'

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # bakery
+
+## 发布程序步骤
+1、 前端工程打包npm run build
+
+2、 文件拷贝   ./release.sh
+
+3、 ssh链接生产机，进到项目目录（bakery目录）
+
+4、 执行git pull下载代码
+
+5、 重启服务 pm2 restart app
--- release.sh
+++ release.sh
@@ -3,9 +3,9 @@ rm bakeryApi/public/static -r
 cp bakeryWeb/dist/index.html bakeryApi/public
 cp -r bakeryWeb/dist/static bakeryApi/public
 
-scp -r bakeryApi/dbconf ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
-scp -r bakeryApi/public ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
-scp -r bakeryApi/routes ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
-scp -r bakeryApi/view ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
-scp bakeryApi/app.js ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
-scp bakeryApi/package.json ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
+#scp -r bakeryApi/dbconf ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
+#scp -r bakeryApi/public ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
+#scp -r bakeryApi/routes ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
+#scp -r bakeryApi/view ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
+#scp bakeryApi/app.js ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
+#scp bakeryApi/package.json ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
