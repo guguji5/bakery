9c9b23462bddff81063c1f42bf9277ba4a4dda54
guguji <kuan.du@grandinsight.com>
2017-08-08 17:37:23 +0800

    update_release

--- release.sh
+++ release.sh
@@ -9,3 +9,9 @@ cp -r bakeryWeb/dist/static bakeryApi/public
 #scp -r bakeryApi/view ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
 #scp bakeryApi/app.js ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
 #scp bakeryApi/package.json ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
+
+git add bakeryApi/public
+msg='public is commited auto in '`date "+%Y-%m-%d %H:%M:%S"`
+git commit -m $msg bakeryApi/public
+git pull
+git push
