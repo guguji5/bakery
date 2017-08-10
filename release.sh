ee288477786056b63d3d10e72edbedbb8db47bf4
guguji <kuan.du@grandinsight.com>
2017-08-10 16:48:32 +0800

    更新shell的注释

--- release.sh
+++ release.sh
@@ -17,7 +17,7 @@ cp -r bakeryWeb/dist/static bakeryApi/public
 #scp bakeryApi/package.json ubuntu@52.15.185.161:/home/ubuntu/bakery/bakeryApi/
 
 git add bakeryApi/public
-msg='public is commited auto in '`date "+%Y-%m-%d %H:%M:%S"`
+msg='the public folder is commited auto in '`date "+%Y-%m-%d %H:%M:%S"`
 git commit -m "$msg" bakeryApi/public
 git pull
 git push
