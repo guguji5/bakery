7fe756a105bb05957e52f5a9cd53dbad90162c05
guguji <kuan.du@grandinsight.com>
2017-08-11 17:37:10 +0800

    https-->http

--- bakeryWeb/src/service/index.js
+++ bakeryWeb/src/service/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import store from '../store/'
-if(window.location.origin!="http://52.15.185.161:3000" && window.location.origin!="https://www.guguji.top:3000"){
+if(window.location.origin!="http://52.15.185.161:3000" && window.location.origin!="http://www.guguji.top:3000"){
     axios.defaults.baseURL = 'http://localhost:3000';
 }
 
